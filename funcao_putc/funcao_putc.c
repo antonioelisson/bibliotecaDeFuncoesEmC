5127726a62990865e7403b76e9528d909e63a0f2
Elisson Antonio da Silva <elissonsilva.dev@gmail.com>
2024-11-27 13:46:21 -0300

    add função putc

---
+++ funcao_putc/funcao_putc.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+main(){
+	FILE *p;
+	char frase[100];
+	int i;
+	
+	p = fopen("frase.txt", "w");
+	
+	if(p == NULL){
+		printf("Erro ao abrir o arquivo");	
+		exit(1);//retorna valor diferente de 0
+	}	
+	else{
+		printf("Digite sua frase: ");
+		gets(frase);
+		
+		for(i = 0; i < (sizeof(frase)); i++)
+			putc(frase[i], p);
+	}
+	fclose(p);
+}
